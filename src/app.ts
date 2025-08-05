dfbcfb2441366bd0e93b28a03aaa8a53827ebdc7
Barbod H. <ph.barbod@gmail.com>
2025-08-05 14:59:48 -0400

    Allow CORS

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import processRoutes from "./routes/processRoutes";
+import cors from "cors";
 
 const baseUrl = "/api/v1";
 
 const app = express();
+
+app.use(cors()); // TODO: Configure appropriate CORS
 
 app.use(express.json());
 app.use(baseUrl + "/process", processRoutes);
