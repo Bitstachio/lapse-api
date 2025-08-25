5323e83b8c53240c0ddd13d480b36afe231ae0bd
BarbodH <habibi.k.barbod@gmail.com>
2025-08-24 22:25:44 -0400

    Implement basic process controller class

--- src/controllers/process-controller.ts
+++ src/controllers/process-controller.ts
@@ -13,6 +13,34 @@ import { IntervalRepository } from "../repositories/interval-repository";
 import { ClientService } from "../services/client-service";
 import { ClientRepository } from "../repositories/client-repository";
 
+export class ProcessController {
+  constructor(
+    private readonly processService: ProcessService,
+    private readonly intervalService: IntervalService,
+    private readonly clientService: ClientService,
+  ) {}
+
+  create(
+    req: Request<{ clientName: string }, {}, IProcessCreateDto>,
+    res: Response<IApiResponse<IProcessGetDto>>,
+  ) {
+    try {
+      const service = new ProcessService(
+        new ProcessRepository(db),
+        new IntervalService(new IntervalRepository(db)),
+        new ClientService(new ClientRepository(db)),
+      );
+      const result = service.create(req.params.clientName, req.body);
+      res.status(201).json({ success: true, message: "Process created", data: result });
+    } catch (error) {
+      if (error instanceof InvalidProcessActionError) {
+        return res.status(400).json({ success: false, message: error.message, error: error.serialize() });
+      }
+      return internalServerError(res);
+    }
+  }
+}
+
 export const testProcess = (_: Request, res: Response) => {
   res.status(200).json({ message: "Process test endpoint is working" });
 };
@@ -25,26 +53,6 @@ export const getProcess = (
     const status = processService.getStatus(req.params.clientId);
     res.status(200).json({ success: true, ...(!status && { message: "No active process" }), data: status });
   } catch (error) {
-    return internalServerError(res);
-  }
-};
-
-export const createProcess = (
-  req: Request<{ clientName: string }, {}, IProcessCreateDto>,
-  res: Response<IApiResponse<IProcessGetDto>>,
-) => {
-  try {
-    const service = new ProcessService(
-      new ProcessRepository(db),
-      new IntervalService(new IntervalRepository(db)),
-      new ClientService(new ClientRepository(db)),
-    );
-    const result = service.create(req.params.clientName, req.body);
-    res.status(201).json({ success: true, message: "Process created", data: result });
-  } catch (error) {
-    if (error instanceof InvalidProcessActionError) {
-      return res.status(400).json({ success: false, message: error.message, error: error.serialize() });
-    }
     return internalServerError(res);
   }
 };
--- src/routes/process-routes.ts
+++ src/routes/process-routes.ts
@@ -1,22 +1,39 @@
 import { Router } from "express";
 import {
-  createProcess,
-  getProcess,
   extendProcess,
+  finishProcess,
+  getProcess,
   pauseProcess,
+  ProcessController,
   resumeProcess,
   testProcess,
-  finishProcess,
 } from "../controllers/process-controller";
+import { ProcessRepository } from "../repositories/process-repository";
+import db from "../db/connection";
+import { ClientRepository } from "../repositories/client-repository";
+import { IntervalRepository } from "../repositories/interval-repository";
+import { ClientService } from "../services/client-service";
+import { IntervalService } from "../services/interval-service";
+import { ProcessService } from "../services/process-service";
 
 const router = Router();
 
+const clientRepository = new ClientRepository(db);
+const intervalRepository = new IntervalRepository(db);
+const processRepository = new ProcessRepository(db);
+
+const clientService = new ClientService(clientRepository);
+const intervalService = new IntervalService(intervalRepository);
+const processService = new ProcessService(processRepository, intervalService, clientService);
+
+const processController = new ProcessController(processService, intervalService, clientService);
+
 router.get("/test", testProcess);
 router.get("/status/:clientId", getProcess);
-router.post("/start/:clientName", createProcess);
+router.post("/start/:clientName", processController.create);
 router.patch("/pause/:clientId", pauseProcess);
 router.patch("/resume/:clientId", resumeProcess);
 router.patch("/extend/:clientId", extendProcess);
-router.patch("/finish/:clientId", finishProcess)
+router.patch("/finish/:clientId", finishProcess);
 
 export default router;
