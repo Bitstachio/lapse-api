461b4b05fc9b6f3ffbe76b6491902b4537257ee9
BarbodH <habibi.k.barbod@gmail.com>
2025-09-06 20:42:15 -0400

    Centralize error status code handling

--- src/errors/process/no-process-error.ts
+++ src/errors/process/no-process-error.ts
@@ -2,6 +2,6 @@ import { BaseServiceError } from "./base-service-error";
 
 export class NoProcessError extends BaseServiceError {
   constructor(message: string = "Process does not exist") {
-    super(NoProcessError.name, message, "NO_PROCESS");
+    super(NoProcessError.name, message, "PROCESS_NOT_FOUND");
   }
 }
--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,17 +1,9 @@
 import { ErrorRequestHandler } from "express";
 import { BaseServiceError } from "../errors/process/base-service-error";
+import { errorStatusCodes } from "../constants/api";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, _) => {
-  if (error instanceof BaseServiceError) {
-    let status = 500;
-
-    if (error.code === "CLIENT_NOT_FOUND") status = 404;
-    if (error.code === "INVALID_ARGUMENT") status = 400;
-    if (error.code === "NO_PROCESS") status = 400;
-
-    return res.status(status).json(error.serialize());
-  }
-
   console.error(error);
+  if (error instanceof BaseServiceError) return res.status(errorStatusCodes[error.code]).json(error.serialize());
   return res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
 };
