78899699cadb54f0c7fc5c99ee5537dffab9f53e
BarbodH <habibi.k.barbod@gmail.com>
2025-09-06 21:54:00 -0400

    Statically type middleware error handler response

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,9 +1,20 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
 import { BaseServiceError } from "../errors/process/base-service-error";
 import { errorStatusCodes } from "../constants/api";
+import { IApiResponse } from "../types/api";
 
-export const errorHandler: ErrorRequestHandler = (error, req, res, _) => {
+export const errorHandler: ErrorRequestHandler = (error, _req, res: Response<IApiResponse>, _next) => {
   console.error(error);
-  if (error instanceof BaseServiceError) return res.status(errorStatusCodes[error.code]).json(error.serialize());
-  return res.status(500).json({ code: "INTERNAL_SERVER_ERROR" });
+  if (error instanceof BaseServiceError) {
+    return res.status(errorStatusCodes[error.code]).json({
+      success: false,
+      message: error.message,
+      error: error.serialize(),
+    });
+  }
+  return res.status(500).json({
+    success: false,
+    message: "Internal server error",
+    error: { code: "INTERNAL_ERROR" },
+  });
 };
